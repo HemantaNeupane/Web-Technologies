faa4af91f0b8a360d1e9ee5d0c0607b1d3bac85e
Hemanta Neupane <np034acs220153@heraldcollege.edu.np>
2023-01-23 22:45:22 +0545

    week 10

---
+++ Week 10/js/query.js
@@ -0,0 +1,9 @@
+var select=document.querySelectorAll("p");
+
+for(var i=0;i<select.length;i++){
+    select[i].style.backgroundColor="beige";
+}
+
+document.querySelector("p.hello").style.backgroundColor="blue";
+
+
---
+++ Week 10/js/variables.js
@@ -0,0 +1,20 @@
+var name="hululululu";
+name=123
+
+let name1="Gululululu";
+name1=345
+
+const name2="Sushmita";
+
+console.log(name,name1,name2)
+console.log(typeof(name))
+
+// document.getElementsByClassName("paragraph")[0].style.backgroundColor='blue';
+
+document.getElementsByClassName("paragraph")[0].style.cssText="background:blue; color:white; font-size:20px";
+
+document.getElementsByClassName("paragraph")[1].innerHTML="Hello Madam, Paichan kon. Bole toh chotta don !!!";
+
+document.getElementsByClassName("h1")[0].innerHTML="Hahahaha hehehehe hululululul";
+
+
